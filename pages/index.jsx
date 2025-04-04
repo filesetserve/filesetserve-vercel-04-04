c2b5fa94228de5023011a3bd52ab0a982c3c3da6
Allen Young <ayoung29@rogers.com>
2025-04-04 16:16:01 -0400

    Added working homepage to index.jsx

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+      <div style={{ padding: "2rem", textAlign: "center" }}>
+        <h1>Welcome to FileSetServe</h1>
+        <p>This is the homepage (index.jsx)</p>
+      </div>
+    );
+  }
+  
